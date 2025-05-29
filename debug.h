72efea1dcb9629199f9490a6d86eba84eb8d5a31
Alex Yu Chen Ni <alexniyuchen@gmail.com>
2025-05-29 02:45:31 -0400

    move circular_buffer.h to lib; implement debug.h

--- debug.h
+++ debug.h
@@ -2,9 +2,22 @@
 #define DEBUG_H
 
 #ifndef NDEBUG
-#define PRINT(...) printf(__VA_ARGS__)
-#else
-#define PRINT(...) ((void)0)
+#include <iostream>
+#include <utility> // for std::forward
+#include <mutex>
 #endif /* NDEBUG */
+
+#ifndef NDEBUG
+#define DPRINT(...) DebugPrint(__VA_ARGS__)
+#else
+#define DPRINT(...) ((void)0)
+#endif /* NDEBUG */
+
+template <typename... Args>
+void DebugPrint(Args&&... args) {
+    static std::mutex mut_debug; // ensure thread-safe output
+    std::lock_guard<std::mutex> lock(mut_debug); // lock the mutex for thread safety
+    (std::cerr << ... << args) << std::endl;  // fold expression (C++17+)
+}
 
 #endif /* DEBUG_H */
